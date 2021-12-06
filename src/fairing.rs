59c43aeaf3519af250f28e4e0135f54acd3c1c73
Christof Weickhardt <christof@weickhardt.ch>
2021-12-06 23:07:25 +0000

    test: fixing doc examples

--- src/fairing.rs
+++ src/fairing.rs
@@ -48,7 +48,7 @@ lazy_static! {
 /// use rocket_async_compression::Compression;
 ///
 /// fn main() {
-///     rocket::ignite()
+///     rocket::build()
 ///         // ...
 ///         .attach(Compression::fairing())
 ///         // ...
@@ -69,7 +69,7 @@ impl Compression {
     /// use rocket_async_compression::Compression;
     ///
     /// fn main() {
-    ///     rocket::ignite()
+    ///     rocket::build()
     ///         // ...
     ///         .attach(Compression::fairing())
     ///         // ...
